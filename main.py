f8131a60244e226cd6747c47624408c4a377e98d
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-08 15:43:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+Python is a high-leval programming language Which is easy to use and simple and 
+is one of the open sourse programming language is used is many applications 
+such as Uber,Dropbox,Google and etc for your knowledge Guido van Rossum is 
+the creator of python.
